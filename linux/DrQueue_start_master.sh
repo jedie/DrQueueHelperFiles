2e86228e5331bbbee2ecb7ba12c1195be0144908
JensDiemer <git@jensdiemer.de>
2015-07-31 14:04:25 +0200

    rename existing log files before start the master

--- linux/DrQueue_start_master.sh
+++ linux/DrQueue_start_master.sh
@@ -5,6 +5,18 @@ source bin/activate
 SETTINGS_FILE=~/DrQueue_env/set_settings
 echo "Read settings from: ${SETTINGS_FILE}"
 source "${SETTINGS_FILE}"
+
+# rename existing log files:
+cd ${DRQUEUE_ROOT}/logs/
+find . -name '*.log' -print0 | while IFS= read -r -d $'\0' log; do
+    log=$(basename "${log}")
+    new_filename=$(date -r "${log}" +%Y%m%d-%H%M)-${log}
+    (
+        set -x
+        mv "${log}" "${new_filename}"
+    )
+done
+echo ""
 (
     set -x
     drqueue -v --no-ssh master
