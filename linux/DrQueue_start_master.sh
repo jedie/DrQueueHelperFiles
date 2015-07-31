a89c6e5235209f0224856c5f79162a8831b460b1
JensDiemer <git@jensdiemer.de>
2015-07-31 16:10:06 +0200

    bind to current IP and quit old instances before start

--- linux/DrQueue_start_master.sh
+++ linux/DrQueue_start_master.sh
@@ -22,9 +22,29 @@ for log in ${LOGS[@]}; do
     fi
 done
 echo ""
+
+
+cd ${DRQUEUE_ROOT}/ipython/profile_default/pid
+for pidfile in *.pid; do
+    echo "quit ${pidfile}"
+    pid=$(cat ${pidfile})
+    (
+        set -x
+        kill -QUIT ${pid}
+    )
+done
+
+echo ""
+
+# Get own ip address:
+ETH=$(dmesg | grep -Eo 'eth[[:digit:]]+' | tail -n1)
+IP=$(ifconfig ${ETH} | head -n2 | tail -n1 | cut -d: -f2 | cut -d" " -f1)
+
 (
     set -x
     { echo "---------------------------------------------------"; } 2>/dev/null
+
+    export DRQUEUE_MASTER=${IP}
 
     drqueue -v --no-ssh master
 
